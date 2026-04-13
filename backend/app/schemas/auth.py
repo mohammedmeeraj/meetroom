f6a753359efd609ddfb22923506a29f8821d716b
Mohammed Meeraj <meerajmohammed743@gmail.com>
2026-04-13 11:22:04 +0530

    feat(auth): add pydantic schemas for user authentication

---
+++ backend/app/schemas/auth.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: str
+    is_active: bool
+    crated_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+    user: UserOut
