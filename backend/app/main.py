6e89cb53efb724d0da4bf3f9211da53b094a8c50
Mohammed Meeraj <meerajmohammed743@gmail.com>
2026-04-13 12:52:07 +0530

    feat: include auth router

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI, Depends
 from typing import Annotated
 from .dependencies import get_settings
 from pydantic_settings import BaseSettings
+from app.routers import auth
+
 
 app = FastAPI(
-    title=Annotated[BaseSettings, Depends(get_settings)],
+    title="MeetRoom",
     description="Anonymous video meeting backend",
     version="1.0.0",
 )
+
+# ---Routers----------------------------------------------------------------
+app.include_router(auth.router, prefix="/api")
 
 @app.get("/")
 async def root(settings: Annotated[BaseSettings, Depends(get_settings)]):
