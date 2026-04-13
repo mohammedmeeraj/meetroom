931dbac60db8917803687f3429ffab8085ece574
Mohammed Meeraj <meerajmohammed743@gmail.com>
2026-04-13 15:42:15 +0530

    feat: add get_current_user and get_currect_active_user dependecies

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -4,8 +4,11 @@ from fastapi.security import OAuth2PasswordBearer
 from datetime import timedelta, datetime, timezone
 from app.dependencies import get_settings
 from typing import Optional
+from app.database import get_db
 from sqlalchemy.orm import Session
 from app.models.user import User
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
 
 settings = get_settings()
 
@@ -13,7 +16,34 @@ password_hash = PasswordHash.recommended()
 
 DUMMY_HASH = password_hash.hash("dummypassword")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/login")
+
+# ----Password helpers-------------------------------------------
+def hash_password(password) -> str:
+    return password_hash.hash(password)
+
+def verify_password(plain_password, hashed_password) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
+
+# ----JWT helpers-------------------------------------------------
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + (
+        expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def decode_token(token: str) -> Optional[dict]:
+    try:
+        return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    
+    except JWTError:
+        return None
+    
+# ---FASTAPI dependency - current authenticated user ----------------------------------
 
 def authenticate_user(db: Session, email: str, password: str) -> Optional[dict]:
     user = db.query(User).filter(User.email == email).first()
@@ -28,25 +58,34 @@ def authenticate_user(db: Session, email: str, password: str) -> Optional[dict]:
     
     return user
 
-# ----Password helpers-------------------------------------------
-def hash_password(password) -> str:
-    return password_hash.hash(password)
-
-def verify_password(plain_password, hashed_password) -> bool:
-    return password_hash.verify(plain_password, hashed_password)
-
-# ----JWT helpers-------------------------------------------------
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (
-        expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Annotated[Session, Depends(get_db)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
-def decode_token(token: str) -> Optional[dict]:
     try:
-        return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        payload = decode_token(token)
+        if payload is None:
+            raise credentials_exception
+        
+        user_id: int = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+        
+        user = db.query(User).filter(User.id == int(user_id)).first()
+
+        if user is None:
+            raise credentials_exception
+        
+        return user
+        
     except JWTError:
-        return None
-    
+        raise credentials_exception
+
+def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+    if not current_user.is_active:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Inactive user")
+
+    return current_user
